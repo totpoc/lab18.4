55dd4b64f44815c97cbc64cd63199378ccffc8e3
spais <maksim.ryabcew@gmail.com>
2021-03-22 11:08:56 +0500

    Добавьте файлы проекта.

---
+++ lab18.4/triad.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class TRIAD
+{
+	int _first, _second, _third;
+
+public:
+
+	TRIAD();
+	TRIAD(int first, int second, int third);
+	TRIAD(const TRIAD& temp);
+	~TRIAD(){}
+
+	void setFirst(int first);
+	void setSecond(int second);
+	void setThird(int third);
+
+	int getFirst();
+	int getSecond();
+	int getThird();
+
+	friend bool     operator==  (const TRIAD& c1, const TRIAD& c2);
+	friend bool     operator!=  (const TRIAD& c1, const TRIAD& c2);
+	friend istream& operator >> (istream& in, TRIAD& temp);
+	friend ostream& operator << (ostream& out, TRIAD& temp);
+};
